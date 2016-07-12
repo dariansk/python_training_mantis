5b70963a2b7b8f0ff10aceb18efaa1e86ccb9899
dariansk <103203@gmail.com>
2016-07-13 00:56:05 +0600

    Добавлена замена конфиг-файла по FTP

--- conftest.py
+++ conftest.py
@@ -3,8 +3,8 @@ import json
 import os.path
 from fixture.application import Application
 import importlib
-import jsonpickle
 from fixture.db import Dbfixture
+import ftputil
 
 fixture = None
 target = None
@@ -20,25 +20,53 @@ def load_config(file):
 
 
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     global target
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
-    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
+        fixture = Application(browser=browser, base_url=config['web']["baseUrl"])
     return fixture
 
 
 @pytest.fixture(scope="session")
-def db(request):
-    db_config = load_config(request.config.getoption("--target"))['db']
-    dbfixture = Dbfixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+def configure(request):
+    return load_config(request.config.getoption("--target"))
+
+
+@pytest.fixture(scope="session")
+def db(request, config):
+    dbfixture = Dbfixture(host=config['db']["host"], name=config['db']["name"], user=config['db']["user"], password=config['db']["password"])
     def fin():
         dbfixture.destroy()
     request.addfinalizer(fin)
     return dbfixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile('config_inc.php.bak'):
+            remote.remove('config_inc.php.bak')
+        if remote.path.isfile('config_inc.php'):
+            remote.rename('config_inc.php', 'config_inc.php.bak')
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile('config_inc.php.bak'):
+            if remote.path.isfile('config_inc.php'):
+                remote.remove('config_inc.php')
+            remote.rename('config_inc.php.bak', 'config_inc.php')
+
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -61,22 +89,12 @@ def pytest_addoption(parser):
     parser.addoption("--check_ui", action="store_true")
 
 
-def pytest_generate_tests(metafunc):
-    for fixture in metafunc.fixturenames:
-        if fixture.startswith("data_"):
-            testdata = load_from_module(fixture[5:])
-            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-        elif fixture.startswith("json_"):
-            testdata = load_from_json(fixture[5:])
-            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-
-
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
 
 
-def load_from_json(file):
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
-        return jsonpickle.decode(f.read())
+#def load_from_json(file):
+#    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+#        return jsonpickle.decode(f.read())
 
 
